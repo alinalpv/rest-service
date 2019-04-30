aada23905a9ba2b905a013c99cfa1e78fda9b899
Alina L <lipovanalina17@gmail.com>
2019-04-30 18:43:38 +0100

    remove unused variables

--- src/main/java/hello/UserController.java
+++ src/main/java/hello/UserController.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.concurrent.atomic.AtomicLong;
-
 @Controller
 public class UserController {
 
